4d4586157e58c92ba1593c9b219b575056012f11
RegMain <kharkov.ivan.an@gmail.com>
2026-09-24 18:38:46 +0300

    Feature: complete tests for basic functions

---
+++ lab_01/tests/test_converter_negative.py
@@ -0,0 +1,36 @@
+import pytest
+from toolkit.converter import convert
+from toolkit.errors import (
+    UnknownUnitError,
+    IncompatibleUnitsError,
+    InvalidValueError
+)
+
+
+def test_unknown_unit_1():
+    with pytest.raises(UnknownUnitError):
+        convert("km", "ft", 10)
+
+def test_unknown_unit_2():
+    with pytest.raises(UnknownUnitError):
+        convert("c", "r", 5.3)
+
+def test_incompatible_units_1():
+    with pytest.raises(IncompatibleUnitsError):
+        convert("c", "g", 3.14)
+
+def test_incompatible_units_2():
+    with pytest.raises(IncompatibleUnitsError):
+        convert("m", "kg", 103)
+
+def test_invalid_value_1():
+    with pytest.raises(InvalidValueError):
+        convert("m", "km", "something")
+
+def test_invalid_value_2():
+    with pytest.raises(InvalidValueError):
+        convert("c", "f", -500)
+
+
+
+
---
+++ lab_01/tests/test_converter_positive.py
@@ -0,0 +1,13 @@
+import pytest
+from toolkit.converter import convert
+
+
+@pytest.mark.parametrize("from_unit, to_unit, value, expected_result", [
+    ["km", "m", 100, 100000.0],
+    ["m", "km", 5250.0, 5.25],
+    ["f", "c", -40, -40.0],
+    ["k", "c", 0, -273.15],
+    ["kg", "g", 0.3223, 0.3223*1000.0]
+])
+def test_calculator_positive(from_unit: str, to_unit: str, value: float, expected_result: float):
+    assert convert(from_unit, to_unit, value) == expected_result
